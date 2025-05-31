af973d0934310b87562cf3fc345c2dfe7c280bff
Jose Luis <jlgomezangarita@gmail.com>
2025-05-30 21:53:34 -0500

    fix: bug of icon flotanting

--- src/app/components/FloatingCartButton.tsx
+++ src/app/components/FloatingCartButton.tsx
@@ -3,50 +3,77 @@
 import Link from 'next/link';
 import { FiShoppingCart } from 'react-icons/fi';
 import { motion, useAnimation } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from '../styles/index.module.scss';
 
 const FloatingCartButton: React.FC = () => {
   const controls = useAnimation();
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
   const [isDragging, setIsDragging] = useState(false);
+
 
   useEffect(() => {
     if (!isDragging) {
       controls.start({
-        y: [0, -5, 0, 5, 0], // movimiento más corto (-5 a 5 en lugar de -10 a 10)
+        y: [0, -5, 0, 5, 0],
         transition: {
-          duration: 3,
+          duration: 9,
           repeat: Infinity,
           ease: 'easeInOut',
         },
       });
-    } else {
-      controls.stop();
     }
   }, [isDragging, controls]);
 
-  const handleDragEnd = () => {
+  const handleDragStart = () => {
+    setIsDragging(true);
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    controls.stop(); 
+  };
+
+  const handleDragEnd = (_: any, info: any) => {
     setIsDragging(false);
-    controls.start({
-      top: 40,  // <-- Bajamos más el botón (antes era 20)
-      right: 30,
-      x: 0,
-      y: 0,
-      transition: { type: 'spring', stiffness: 300 },
-    });
+
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+
+    timeoutRef.current = setTimeout(() => {
+      controls.start({
+        x: 0,
+        y: 0,
+        transition: {
+          type: 'spring',
+          stiffness: 200,
+          damping: 60,
+        },
+      }).then(() => {
+        controls.start({
+          y: [0, -5, 0, 5, 0],
+          transition: {
+            duration: 9,
+            repeat: Infinity,
+            ease: 'easeInOut',
+          },
+        });
+      });
+    }, 2000);
   };
 
   return (
     <motion.div
       className={styles.floatingCart}
       drag
-      dragMomentum={false}
-      dragElastic={0.1}
-      onDragStart={() => setIsDragging(true)}
+      dragMomentum
+      dragElastic={3}
+      onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
       animate={controls}
-      initial={{ top: 20, right: 20 }} // <-- También acá lo dejamos en 100
-      style={{ position: 'fixed' }}
+      initial={{  }}
+      style={{
+        position: 'fixed',
+        top: 40,
+        right: 30,
+      }}
     >
       <Link href="/cart">
         <FiShoppingCart size={30} />
